c2d1050b5c1e17b13b58cef60f485e01d51931bc
Nicole Rue <nicolerue92@gmail.com>
2023-06-07 15:01:35 -0700

    Add three methods

--- .arrays.js
+++ .arrays.js
@@ -1,3 +1,13 @@
 var fruits = ["apples", "bananas", "oranges", "kiwis"];
 var numbers = [1,2,3,4];
-var booleans = [true, false, false, true]
+var booleans = [true, false, false, true];
+
+fruits.push("grapes");
+console.log(fruits);
+
+numbers.slice(0,1); // expecting output = 3,4
+console.log(numbers);
+
+booleans.unshift("true"); // expecting two trues at the start 
+console.log(booleans)
+
